cd38142e6adf26946bc318854ec450e65b35f181
OnurKader <onurkader@protonmail.com>
2020-03-20 23:35:42 +0300

    Adding remote to GitHub

--- Maval.hpp
+++ Maval.hpp
@@ -1,14 +1,17 @@
 #pragma once
 
+#include <cfloat>
+#include <climits>
 #include <cstdint>
 #include <cstdio>
 #include <cstring>
+#include <limits>
 #include <stack>
 #include <string>
 
 namespace OK
 {
-enum class Operator
+enum class Operator : uint8_t
 {
 	Nop,
 	Addition,
@@ -49,9 +52,29 @@ class UnaryExpression : public Expression
 		switch(m_operator)
 		{
 			case Operator::Subtraction: return -m_rhs;
+			// TODO Either static_cast to int or determine the output with a ternary
+			case Operator::LogicalNot: return !static_cast<int>(m_rhs);
 			default: return m_rhs;
 		}
 	}
+};
+
+class BinaryExpression : public Expression
+{
+	public:
+	BinaryExpression(Operator op, double lhs, double rhs) : Expression(op, rhs), m_lhs(lhs) {}
+	~BinaryExpression() override {}
+	double evaluate() override
+	{
+		switch(m_operator)
+		{
+			case Operator::Nop: return DBL_MAX;
+			default: fprintf(stderr, "I'm sorry Onur, I'm afraid I can't do that\n");
+		}
+	}
+
+	private:
+	double m_lhs;
 };
 
 class Maval
